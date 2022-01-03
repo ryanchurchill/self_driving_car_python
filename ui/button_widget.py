3871fcb6c1d38a51b32be33ec781c5f045597bd4
Ryan Churchill <ryan.churchill@gmail.com>
2022-01-02 20:33:39 -0800

    bit of cleanup

--- ui/button_widget.py
+++ ui/button_widget.py
@@ -1,18 +1,44 @@
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
-
 class ButtonWidget(QWidget):
-    def __init__(self):
+    def __init__(self, height):
         super(ButtonWidget, self).__init__()
 
         layout = QHBoxLayout(self)
 
-        play_button = QPushButton('Play')
-        pause_button = QPushButton('Pause')
+        buttons = []
 
-        layout.addWidget(play_button)
-        layout.addWidget(pause_button)
+        btn_play = QPushButton('Play')
+        buttons.append(btn_play)
+        btn_pause = QPushButton('Pause')
+        buttons.append(btn_pause)
+        btn_save_brain = QPushButton('Save Brain')
+        buttons.append(btn_save_brain)
+        btn_load_brain = QPushButton('Load Brain')
+        buttons.append(btn_load_brain)
+        btn_save_sand = QPushButton('Save Sand')
+        buttons.append(btn_save_sand)
+        btn_load_sand = QPushButton('Load Sand')
+        buttons.append(btn_load_sand)
+
+        for button in buttons:
+            button.setFixedHeight(height)
+            layout.addWidget(button)
+
+        # btn_play.setFixedHeight(height)
+        # btn_play.setFixedHeight(height)
+        # btn_play.setFixedHeight(height)
+        # btn_play.setFixedHeight(height)
+        # btn_play.setFixedHeight(height)
+        # btn_play.setFixedHeight(height)
+        #
+        # layout.addWidget(btn_play)
+        # layout.addWidget(btn_pause)
+        # layout.addWidget(btn_save_brain)
+        # layout.addWidget(btn_load_brain)
+        # layout.addWidget(btn_save_sand)
+        # layout.addWidget(btn_load_sand)
 
         # make background blue
         # container = QWidget
@@ -20,6 +46,3 @@ class ButtonWidget(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor('blue'))
         self.setPalette(palette)
-
-        # self.setContentsMargins(0,0,0,0)
-        # self.setSpacing(0)
--- ui/main_window.py
+++ ui/main_window.py
@@ -18,49 +18,26 @@ class MainWindow(QMainWindow):
 
         self.car = Car(50, 50)
         self.game_widget = GameWidget(self.car)
+        button_widget = ButtonWidget(BUTTON_PANEL_HEIGHT)
 
         self.setWindowTitle("Self Driving Car")
         self.setFixedSize(QSize(GAME_WIDTH, GAME_HEIGHT+BUTTON_PANEL_HEIGHT))
 
-        # button: QPushButton = QPushButton("Press Me!")
+        self.game_widget.setFixedSize(GAME_WIDTH, GAME_HEIGHT)
 
-
-        # Set the central widget of the Window.
-        # self.setCentralWidget(button)
-
-
-
+        # layout
         central_layout = QVBoxLayout()
-        central_layout.setContentsMargins(0,0,0,0)
+        central_layout.setContentsMargins(0, 0, 0, 0)
         central_layout.setSpacing(0)
 
-        # game_widget = Color('black')
-
-
-
-
         central_layout.addWidget(self.game_widget)
+        central_layout.addWidget(button_widget)
 
-
-        self.game_widget.setFixedSize(GAME_WIDTH, GAME_HEIGHT)
-        # self.game_widget.setContentsMargins(0,0,0,0)
-
-
+        # central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         central_widget.setLayout(central_layout)
 
-        button_widget = ButtonWidget()
-        central_layout.addWidget(button_widget)
-        # button_widget.setFixedSize(GAME_WIDTH, BUTTON_PANEL_HEIGHT)
-
-        # button_widget.resize(GAME_WIDTH, GAME_HEIGHT)
-
-        # temp - button testing
-        # button = QPushButton("Press Me!")
-        # button.setCheckable(True)
-        # button.clicked.connect(self.button_pressed)
-        # central_layout.addWidget(button)
 
     def button_pressed(self):
 
