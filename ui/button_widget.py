9406f9f5f918264c8f39660afa87faa34e36260a
Ryan Churchill <ryan.churchill@gmail.com>
2022-02-12 19:08:11 -0800

    can pause brain

--- ui/button_widget.py
+++ ui/button_widget.py
@@ -8,26 +8,34 @@ class ButtonWidget(QWidget):
     def __init__(self, height, game_widget: GameWidget):
         super(ButtonWidget, self).__init__()
         self.game_widget = game_widget
+        self.brain_on = False
 
         layout = QHBoxLayout(self)
 
         buttons = []
 
         btn_start_brain = QPushButton('Start Brain')
-        buttons.append(btn_start_brain)
         btn_start_brain.clicked.connect(self.btn_start_brain_clicked)
+        buttons.append(btn_start_brain)
+
         btn_pause_brain = QPushButton('Pause Brain')
+        btn_pause_brain.clicked.connect(self.btn_pause_brain_clicked)
         buttons.append(btn_pause_brain)
+
         btn_save_brain = QPushButton('Save Brain')
         buttons.append(btn_save_brain)
+
         btn_load_brain = QPushButton('Load Brain')
         buttons.append(btn_load_brain)
+
         btn_clear_sand = QPushButton('Clear Sand')
         buttons.append(btn_clear_sand)
         btn_clear_sand.clicked.connect(self.btn_clear_clicked)
+
         btn_save_sand = QPushButton('Save Sand')
         btn_save_sand.clicked.connect(self.game_widget.sand.save_to_file)
         buttons.append(btn_save_sand)
+
         btn_load_sand = QPushButton('Load Sand')
         btn_load_sand.clicked.connect(self.btn_load_sand_clicked)
         buttons.append(btn_load_sand)
@@ -58,7 +66,13 @@ class ButtonWidget(QWidget):
         self.setPalette(palette)
 
     def btn_start_brain_clicked(self):
-        self.game_widget.ai_timer.start(100)
+        if self.brain_on == False:
+            self.game_widget.ai_timer.start(100)
+            self.brain_on = True
+
+    def btn_pause_brain_clicked(self):
+        self.game_widget.ai_timer.stop()
+        self.brain_on = False
 
 
     def btn_clear_clicked(self):
